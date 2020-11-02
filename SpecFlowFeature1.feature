3d4ae29ee34ef73428b1f7d6a77dce04dd736874
Fritz <wi19b106@technikum-wien.at>
2020-11-02 18:57:11 +0100

    Projektdateien hinzufügen.

---
+++ SpecFlowFeature1.feature
@@ -0,0 +1,26 @@
+﻿Feature: SpecFlowFeature1
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: return pi Math.PI 
+    Given the object Math.pi
+    When calling pi function
+    Then the result should be exaclty pi
+
+
+Scenario: return Sin from <number>
+    Given <number> and function Math.sinus
+    When passing <number> 
+    Then should return Sin(number)
+
+Scenario: return Cos from <number>
+    Given <number> and function Math.cos
+    When passing <number> 
+    Then should return Sin(number)
+
+Scenario: return Tang from <number>
+    Given <number> and function Math.tang
+    When passing <number> 
+    Then should return Sin(number)
---
+++ SpecFlowFeature1Steps.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Calculator
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        [Given(@"the object Math\.pi")]
+        public void GivenTheObjectMath_Pi()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"(.*) and function Math\.sinus")]
+        public void GivenAndFunctionMath_Sinus(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"(.*) and function Math\.cos")]
+        public void GivenAndFunctionMath_Cos(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"(.*) and function Math\.tang")]
+        public void GivenAndFunctionMath_Tang(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"calling pi function")]
+        public void WhenCallingPiFunction()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"passing (.*)")]
+        public void WhenPassing(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the result should be exaclty pi")]
+        public void ThenTheResultShouldBeExacltyPi()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"should return Sin\(number\)")]
+        public void ThenShouldReturnSinNumber()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
